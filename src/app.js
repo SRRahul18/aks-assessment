938f89f7ccca929ad54da4daa97ce0a6ea83d01d
SRRahul18 <63399361+SRRahul18@users.noreply.github.com>
2025-10-03 21:17:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+let express = require('express');
+let app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Node.js on Kubernetes!');
+app.get('/', function (req, res) {
+  let obj = {
+    endpoints: [
+      "/ping",
+      "/current-date",
+      "/fibo/:n",
+    ]
+  };
+  res.send(obj);
 });
 
-app.listen(port, () => {
-  console.log(`App running on http://0.0.0.0:${port}`);
+app.get('/ping', function (req, res) {
+  res.send("pong");
 });
+
+
+app.get('/current-date', function (req, res) {
+  let obj = {
+    name: "current",
+    value: new Date()
+  };
+  res.send(obj);
+});
+
+app.get('/fibo/:n', function (req, res) {
+  let obj = {
+    name: "fibo",
+    value: fibo(req.params.n)
+  };
+  res.send(obj);
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+function fibo(n) { // 1
+  if (n < 2)
+    return 1;
+  else   return fibo(n - 3) + fibo(n - 1);
+}
